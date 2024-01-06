7097325ede6047a1c7321ec86f49824215d5f595
MalcolmDahling <mac.dahling@gmail.com>
2024-01-06 23:27:50 +0100

    added try-catch

--- getEvents.js
+++ getEvents.js
@@ -2,133 +2,143 @@ const { default: puppeteer } = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
 
-async function getEvents(eventType){
+async function getEvents(eventType) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  try {
+    await page.goto('https://leekduck.com/events/', { waitUntil: 'domcontentloaded' });
+  } catch (err) {
+    console.log(err);
+  }
 
-    await page.goto('https://leekduck.com/events/', {waitUntil: 'domcontentloaded'});
+  let events;
 
-    let events;
-
-    if(eventType === 'current'){
-
-        events = await page.evaluate(() => {
-
-            return Array.from(document.querySelectorAll('body > div > article > div > div > div.events-list.current-events > span > a:not(.hide-event)'), el => el.textContent.replace(/\t|\n/g, '').split('   '));
-        });
+  if (eventType === 'current') {
+    try {
+      events = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('body > div > article > div > div > div.events-list.current-events > span > a:not(.hide-event)'), (el) => el.textContent.replace(/\t|\n/g, '').split('   '));
+      });
+    } catch (err) {
+      console.log(err);
     }
-    else{
+  }
 
-        events = await page.evaluate(() => {
-
-            return Array.from(document.querySelectorAll('body > div > article > div > div > div.events-list.upcoming-events > span > a:not(.hide-event)'), el => el.textContent.replace(/\t|\n/g, '').split('   '));
-        });
+  if (eventType === 'upcoming') {
+    try {
+      events = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('body > div > article > div > div > div.events-list.upcoming-events > span > a:not(.hide-event)'), (el) => el.textContent.replace(/\t|\n/g, '').split('   '));
+      });
+    } catch (err) {
+      console.log(err);
     }
-    
-    //remove empty entries
-    events = events.map(subArr => subArr.filter(str => str.trim() !== ''));
+  }
 
-    //remove spaces at the start of the strings
-    events = events.map(subArr => subArr.map(str => str.trim()));
-    
-    //remove double spaces
-    events = events.map(arr => arr.map(str => str.replace(/\s{2,}/g, ' ').trim()));
+  //remove empty entries
+  events = events.map((subArr) => subArr.filter((str) => str.trim() !== ''));
 
-    //turn into objects
-    events = events.map(event => {
-        return {
-            type:event[0],
-            name:event[1],
-            date:event[2],
-            endsOrStarts:event[3],
-            color:'',
-            img:'',
-            url:''
-        };
-    });
+  //remove spaces at the start of the strings
+  events = events.map((subArr) => subArr.map((str) => str.trim()));
 
-    
-    if(eventType === 'current'){
+  //remove double spaces
+  events = events.map((arr) => arr.map((str) => str.replace(/\s{2,}/g, ' ').trim()));
 
-        const images = await page.evaluate(() => {
+  //turn into objects
+  events = events.map((event) => {
+    return {
+      type: event[0],
+      name: event[1],
+      date: event[2],
+      endsOrStarts: event[3],
+      color: '',
+      img: '',
+      url: '',
+    };
+  });
 
-            return Array.from(document.querySelectorAll(`body > div > article > div > div > div.events-list.current-events > span > a:not(.hide-event) > div > div.event-item > span > img`), el => el.src);
+  if (eventType === 'current') {
+    try {
+      const images = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll(`body > div > article > div > div > div.events-list.current-events > span > a:not(.hide-event) > div > div.event-item > span > img`), (el) => el.src);
+      });
+
+      const colors = await page.evaluate(() => {
+        const elements = Array.from(document.querySelectorAll('body > div > article > div > div > div.events-list.current-events > span > a:not(.hide-event) > div'), (el) => el);
+
+        let styles = elements.map((el) => {
+          return window.getComputedStyle(el).backgroundColor;
         });
 
-        const colors = await page.evaluate(() => {
+        return styles;
+      });
 
-            const elements = Array.from(document.querySelectorAll("body > div > article > div > div > div.events-list.current-events > span > a:not(.hide-event) > div"), el => el);
+      const urls = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll(`body > div > article > div > div > div.events-list.current-events > span > a:not(.hide-event)`), (el) => el.href);
+      });
 
-            let styles = elements.map(el => {
+      for (let i = 0; i < events.length; i++) {
+        events[i].img = images[i];
+        events[i].color = colors[i];
+        events[i].url = urls[i];
+      }
 
-                return window.getComputedStyle(el).backgroundColor;
-            });
+      const filePath = path.join(__dirname, 'output', './currentEvents.json');
 
-            return styles
+      if (events.length > 0) {
+        fs.writeFile(filePath, JSON.stringify(events, null, 2), (err) => {
+          if (err) {
+            console.log(err);
+          }
         });
-
-        const urls = await page.evaluate(() => {
-
-            return Array.from(document.querySelectorAll(`body > div > article > div > div > div.events-list.current-events > span > a:not(.hide-event)`), el => el.href);
-        });
-
-        for(let i = 0; i < events.length; i++){
-
-            events[i].img = images[i];
-            events[i].color = colors[i];
-            events[i].url = urls[i];
-        }
-
-        const filePath = path.join(__dirname, 'output', './currentEvents.json');
-
-        fs.writeFile(filePath, JSON.stringify(events), err => {
-
-            if(err){console.log(err);}
-        });
+      }
+    } catch (err) {
+      console.log(err);
     }
-    else{
+  }
 
-        const images = await page.evaluate(() => {
+  if (eventType === 'upcoming') {
+    try {
+      const images = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll(`body > div > article > div > div > div.events-list.upcoming-events > span > a:not(.hide-event) > div > div.event-item > span > img`), (el) => el.src);
+      });
 
-            return Array.from(document.querySelectorAll(`body > div > article > div > div > div.events-list.upcoming-events > span > a:not(.hide-event) > div > div.event-item > span > img`), el => el.src);
+      const colors = await page.evaluate(() => {
+        const elements = Array.from(document.querySelectorAll('body > div > article > div > div > div.events-list.upcoming-events > span > a:not(.hide-event) > div'), (el) => el);
+
+        let styles = elements.map((el) => {
+          return window.getComputedStyle(el).backgroundColor;
         });
 
-        const colors = await page.evaluate(() => {
+        return styles;
+      });
 
-            const elements = Array.from(document.querySelectorAll("body > div > article > div > div > div.events-list.upcoming-events > span > a:not(.hide-event) > div"), el => el);
+      const urls = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll(`body > div > article > div > div > div.events-list.upcoming-events > span > a:not(.hide-event)`), (el) => el.href);
+      });
 
-            let styles = elements.map(el => {
+      for (let i = 0; i < events.length; i++) {
+        events[i].img = images[i];
+        events[i].color = colors[i];
+        events[i].url = urls[i];
+      }
 
-                return window.getComputedStyle(el).backgroundColor;
-            });
+      const filePath = path.join(__dirname, 'output', './upcomingEvents.json');
 
-            return styles
+      if (events.length > 0) {
+        fs.writeFile(filePath, JSON.stringify(events, null, 2), (err) => {
+          if (err) {
+            console.log(err);
+          }
         });
-
-        const urls = await page.evaluate(() => {
-
-            return Array.from(document.querySelectorAll(`body > div > article > div > div > div.events-list.upcoming-events > span > a:not(.hide-event)`), el => el.href);
-        });
-
-        for(let i = 0; i < events.length; i++){
-
-            events[i].img = images[i];
-            events[i].color = colors[i];
-            events[i].url = urls[i];
-        }
-
-        const filePath = path.join(__dirname, 'output', './upcomingEvents.json');
-
-        fs.writeFile(filePath, JSON.stringify(events), err => {
-
-            if(err){console.log(err);}
-        });
+      }
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    await browser.close();
+  await browser.close();
 }
 
 module.exports = {
-    getEvents
-}
+  getEvents,
+};
--- server.js
+++ server.js
@@ -13,20 +13,18 @@ getEvents('upcoming');
 
 //update every 5 minutes
 setInterval(() => {
-    getEvents('current');
-    getEvents('upcoming');
+  getEvents('current');
+  getEvents('upcoming');
 }, 300000);
 
-let options = {root: path.join(__dirname)};
+let options = { root: path.join(__dirname) };
 
 server.get('/currentEvents', (req, res) => {
-
-    res.sendFile('./output/currentEvents.json', options);
+  res.sendFile('./output/currentEvents.json', options);
 });
 
 server.get('/upcomingEvents', (req, res) => {
-
-    res.sendFile('./output/upcomingEvents.json', options);
+  res.sendFile('./output/upcomingEvents.json', options);
 });
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
